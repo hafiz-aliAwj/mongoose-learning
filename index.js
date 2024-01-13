82835db147525ffdc283b90df3b3c31fd762a897
hafiz-aliAwj <hafizaliawj112@gmail.com>
2024-01-13 13:01:55 +0500

    models and schema

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ main().catch(err => console.log(err));
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/learning');
 }
+ // Schema
+
+// const blogSchema = new mongoose.Schema({
+//     title: String, 
+//     author: String,
+//     body: String,
+//     meta: {
+//       votes: Number,
+//       favs: Number
+//     }
+//   });
+
+
+//models
+
+// const Blogs = mongoose.model('Blog', blogSchema);
